4bcd6463d8d4556281a0ca877c5ba4a9562a4598
João Tupinambá <joaoh.tupinamba@gmail.com>
2024-01-17 01:27:51 -0300

    feat: import kafka module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KafkaModule } from './kafka/kafka.module';
 
 @Module({
-  imports: [],
+  imports: [KafkaModule],
   controllers: [AppController],
   providers: [AppService],
 })
